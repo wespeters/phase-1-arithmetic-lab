d880cb068c54a3c18b2cf46e0473e9abb6d02a48
Wesley Peters <wesleypeters@gmail.com>
2023-05-23 09:37:30 -0600

    creates variables, passes tests

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random());
+
+let num3 = 18;
+let num4 = 7;
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20)
